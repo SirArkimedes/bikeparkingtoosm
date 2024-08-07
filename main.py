a721dea21364b5843665a75ff5fb4c503f71e6bd
Andrew Robinson <sirarkimedes@gmail.com>
2024-08-06 21:05:25 -0600

    Allow the ENV to be recreated easily too

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv 
 import osmapi
+
+# loading variables from .env file
+load_dotenv() 
+ 
+# accessing and printing value
+osm_client_id = os.getenv("OSM_CLIENT_ID")
+osm_client_secret = os.getenv("OSM_CLIENT_SECRET")
+
 api = osmapi.OsmApi()
 print(api.NodeGet(123))
