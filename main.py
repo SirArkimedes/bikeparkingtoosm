4ae1f521d2c26a0517f9a203d419751fd415dedf
Andrew Robinson <sirarkimedes@gmail.com>
2024-08-06 22:41:34 -0600

    oauth osmapi

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import os
+from requests_oauth2client import OAuth2Client, OAuth2AuthorizationCodeAuth
+import requests
+import webbrowser
 import csv
+import os
 import sys
 
 
 from dotenv import load_dotenv 
 import osmapi
-
-
-osm_client_id = os.getenv("OSM_CLIENT_ID")
-osm_client_secret = os.getenv("OSM_CLIENT_SECRET")
 
 class ParkingType: 
   SCHOOL_YARD = "Schoolyard"
@@ -37,12 +36,50 @@ class Point:
     self.lon = lon
     self.type = type
 
+def auth_osm() -> osmapi.OsmApi:
+  client_id = os.getenv("OSM_CLIENT_ID")
+  client_secret = os.getenv("OSM_CLIENT_SECRET")
+  # special value for redirect_uri for non-web applications
+  redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  authorization_base_url = "https://master.apis.dev.openstreetmap.org/oauth2/authorize"
+  token_url = "https://master.apis.dev.openstreetmap.org/oauth2/token"
+
+  oauth2client = OAuth2Client(
+      token_endpoint=token_url,
+      authorization_endpoint=authorization_base_url,
+      redirect_uri=redirect_uri,
+      client_id=client_id,
+      client_secret=client_secret,
+      code_challenge_method=None,
+  )
+
+  # open OSM website to authrorize user using the write_api and write_notes scope
+  scope = ["write_api", "write_notes"]
+  az_request = oauth2client.authorization_request(scope=scope)
+  print(f"Authorize user using this URL: {az_request.uri}")
+  webbrowser.open(az_request.uri)
+
+  # create a new requests session using the OAuth authorization
+  auth_code = input("Paste the authorization code here: ")
+  auth = OAuth2AuthorizationCodeAuth(
+      oauth2client,
+      auth_code,
+      redirect_uri=redirect_uri,
+  )
+  oauth_session = requests.Session()
+  oauth_session.auth = auth
+
+  return osmapi.OsmApi(
+    api="https://api06.dev.openstreetmap.org",
+    session=oauth_session
+  )
+
 def setup():
   load_dotenv() 
  
-  api = osmapi.OsmApi()
+  api = auth_osm()
   print(api.NodeGet(123))
-
+  
   points = read_csv()
   print("DONE")
 
@@ -78,6 +115,8 @@ def read_csv() -> list[Point]:
   all_points = []
   with open('data/points.csv', newline='\n') as file:
     reader = csv.reader(file, delimiter=' ', quotechar='|')
+
+    last_point = None
     for row in reader:
         if len(row) > 0 and row[0] == '\"POINT':
           lon_row = row[1]
@@ -96,8 +135,12 @@ def read_csv() -> list[Point]:
             if type_string_to_enum(new_type) != ParkingType.UNKNOWN:
               type_string = new_type
 
-          point = Point(float(lat), float(lon), type_string_to_enum(type_string))
-          all_points.append(point)
+          last_point = Point(
+            float(lat),
+            float(lon),
+            type_string_to_enum(type_string)
+          )
+          all_points.append(last_point)
 
     return all_points
 
